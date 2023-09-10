dd9a8571dca53d2dbfbaabe30b92081cd6259bf8
Peerapong.Pia <Peerapongg1998@gmail.com>
2023-09-10 18:09:22 +0700

    Exercise4_Peerapong_P
    Exercise 4 : ปฏิบัติการทดสอบการใช้งานตัวแปร

---
+++ Exercise4_Peerapong_P.py
@@ -0,0 +1,10 @@
+scoreSub_eng = 70.5
+scoreSub_Business = 88.5
+scoreSub_Coms = 90.0
+scoreSub_compro = 95.5
+
+print('-----Your Score----')
+print('Foundation English  : ',scoreSub_eng ,'คะแนน')
+print('General Business    : ',scoreSub_Business, 'คะแนน')
+print('Introduction to Computer Systems : ',scoreSub_Coms, 'คะแนน')
+print('Computer Programming : ',scoreSub_compro, 'คะแนน')
