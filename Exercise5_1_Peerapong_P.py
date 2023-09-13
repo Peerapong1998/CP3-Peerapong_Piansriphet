c64d074fc0fab0679af7896750518f5e9c6d9605
Peerapong.Pia <Peerapongg1998@gmail.com>
2023-09-13 20:08:44 +0700

    Exercise5_1_Peerapong_P
    HomeWork

---
+++ Exercise5_1_Peerapong_P.py
@@ -0,0 +1,11 @@
+num1 = int(input('number Frist : '))
+num2 = int(input('number Two   : '))
+sum1 = num1 + num2
+Subtraction = num1 - num2
+multi = num1 * num2
+Division = num1 / num2
+
+print(num1,"+",num2,'=',sum1)
+print(num1,"-",num2,'=',Subtraction)
+print(num1,"*",num2,'=',multi)
+print(num1,"/",num2,'=',int(Division))
---
+++ Exercise5_2_Peerapong_P.py
@@ -0,0 +1,4 @@
+distance = int(input('Please enter distance:  '))
+movingTime = int(input('Please moving time:  '))
+v = distance / movingTime
+print(int(v),'km/h')
