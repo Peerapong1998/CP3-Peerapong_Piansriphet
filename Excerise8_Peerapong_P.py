2c74f134e0876d385fa5e4d89b224e6a65af9008
Peerapong.Pia <Peerapongg1998@gmail.com>
2023-09-15 21:12:45 +0700

    Excerise8_Peerapong_P
    Homework_8

---
+++ Excerise8_Peerapong_P.py
@@ -0,0 +1,31 @@
+# หลังจากผู้ซื้อเลือกเรียบร้อยแล้ว โปรแกรมจะทำการแสดงสรุปราคารวมของรายการสั่งซื้อทั้งหมด
+usernameInput = input("Username : ")
+passwoordInput = input("Password: ")
+
+if usernameInput == 'admin' and passwoordInput == '1234':
+    print("-----------------Welcome to Shop-------------------")
+    print("------------------List menu-------------------------")
+    print("---------------1.Notebook 25000(thb)")
+    print("---------------2.Pc 30000 (thb)")
+    print("---------------3.Macbook  50000(thb)")
+    userSelected = int(input("Choose Product :   "))
+    if userSelected == 1:
+       print("1.Notebook:")
+       unit = int(input("How much  :  "))
+       priceProduct = 25000
+       total = unit * priceProduct
+       print("คุณต้องจ่ายเงินทั้งหมด :  ",total)
+    elif userSelected == 2:
+       print("2.Pc:")
+       unit = int(input("How much :  "))
+       priceProduct = 30000
+       total = unit * priceProduct
+       print("คุณต้องจ่ายเงินทั้งหมด :  ",total)
+
+    elif userSelected == 3:
+       print("3.Macbook:")
+       unit = int(input("How much :  "))
+       priceProduct = 50000
+       total = unit * priceProduct
+       print("คุณต้องจ่ายเงินทั้งหมด :  ",total)
+print("------ขอบคุณที่ใช้บริการร้านเรานะ------------")
