a044bdeacf651c2db4647392960f93a60925503a
Peerapong.Pia <Peerapongg1998@gmail.com>
2024-05-25 20:28:18 +0700

    Calculate function
    Home Work Function

---
+++ FunctionExample.py
@@ -0,0 +1,24 @@
+def addNumber():
+    x = int(input('Please enter a number: '))
+    y = int(input("Please enter a number: "))
+    z  = x + y
+    print(z)
+#----------------------------------------------   
+def addDeleteNumber():
+    x = int(input('Please enter a number: '))
+    y = int(input("Please enter a number: "))
+    z  = x - y
+    print(z)
+#----------------------------------------------
+def addMultiplyNumber():
+    x = int(input('Please enter a number: '))
+    y = int(input("Please enter a number: "))
+    z  = x * y
+    print(z)
+#----------------------------------------------
+def addDivideNumber():
+    x = int(input('Please enter a number: '))
+    y = int(input("Please enter a number: "))
+    z  = x / y
+    print(z)
+#----------------------------------------------
