02f6da57190c61eb074f4734d6ec48c4a984f952
Peerapong.Pia <Peerapongg1998@gmail.com>
2024-05-25 20:31:49 +0700

    Edit single quotes
    Edit single quotes double quotes

--- FunctionExample.py
+++ FunctionExample.py
@@ -1,23 +1,23 @@
 def addNumber():
-    x = int(input('Please enter a number: '))
+    x = int(input("Please enter a number: "))
     y = int(input("Please enter a number: "))
     z  = x + y
     print(z)
 #----------------------------------------------   
 def addDeleteNumber():
-    x = int(input('Please enter a number: '))
+    x = int(input("Please enter a number: "))
     y = int(input("Please enter a number: "))
     z  = x - y
     print(z)
 #----------------------------------------------
 def addMultiplyNumber():
-    x = int(input('Please enter a number: '))
+    x = int(input("Please enter a number: "))
     y = int(input("Please enter a number: "))
     z  = x * y
     print(z)
 #----------------------------------------------
 def addDivideNumber():
-    x = int(input('Please enter a number: '))
+    x = int(input("Please enter a number: "))
     y = int(input("Please enter a number: "))
     z  = x / y
     print(z)
