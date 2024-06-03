900d0559158e3513c0d83077d4adb48b051b0062
Peerapong.Pia <Peerapongg1998@gmail.com>
2024-06-03 18:31:50 +0700

    Design_text
    Add Label  Welcome to Program

--- Lecture99.py
+++ Lecture99.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 def leftClickButton(event):
     height = txtBoxHight.get()
-    weight = txtBoxWegiht.get()
+    weight = txtBoxWight.get()
     BMI = int(weight) / (int(height)/100)** 2
     labelTotal.configure(text=BMI)
     if BMI <= 18.5 :
@@ -17,26 +17,29 @@ def leftClickButton(event):
        labelResult.configure(text="อ้วนมาก")
 
 mainwindows = Tk()
-lebelheight = Label(mainwindows,text='ส่วนสูง(cm.)')
-lebelheight.grid(row=0,column=0)
+
+labelWel = Label(mainwindows,text="Welcome to Program BMI",anchor="center",bg ="#b39ddb",fg="#e0f7fa")
+labelWel.grid(row=0,column=1)
+#-----------ปุ่มส่วนสูง-----------------------
+labelheight = Label(mainwindows,text='ส่วนสูง(cm.) ')
+labelheight.grid(row=1,column=0)
 txtBoxHight = Entry(mainwindows)
-
-txtBoxHight.grid(row=0,column=1)
-lebelweight = Label(mainwindows,text='น้ำหนัก(kg.)')
-lebelweight.grid(row=1,column=0)
-
-txtBoxWegiht = Entry(mainwindows)
-txtBoxWegiht.grid(row=1,column=1)
-calculateButton = Button(mainwindows,text='คำนวณ')
-calculateButton.grid(row=2)
-
-calculateButton.bind('<Button-1>',leftClickButton)
-calculateButton.grid(row=2,column=0)
-
+txtBoxHight.grid(row=1,column=1)
+#-------------ปุ่มน้ำหนัก-----------------------
+labelweight = Label(mainwindows,text='น้ำหนัก(Kg.) ')
+labelweight.grid(row=2,column=0)
+txtBoxWight = Entry(mainwindows)
+txtBoxWight.grid(row=2,column=1)
+#--------------ปุ่มคำนวณ----------------------
+calculateButton = Button(mainwindows,text="คำนวณ")
+calculateButton.grid(row=3)
+calculateButton.bind("<Button-1>",leftClickButton)
+calculateButton.grid(row=3,column=0)
+#---------------แสดงผล-------------------------
 labelTotal = Label(mainwindows,text =" ")
-labelTotal.grid(row=2,column=1)
-
+labelTotal.grid(row=3,column=1)
+#---------------------------------------
 labelResult = Label(mainwindows,text=" ")
-labelResult.grid(row=3,column=1)
-
+labelResult.grid(row=4,column=1)
+#-------------------------------------
 mainwindows.mainloop()
